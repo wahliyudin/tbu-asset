3ec46f8774061cbc28b6ac9a3d5c5e2d1eae8aea
systemdev72 <systemdev72@gmail.com>
2023-09-12 02:15:01 -0700

    HRIS Model

---
+++ app/Models/Employee.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Employee extends Model 
+{
+    use HasFactory;
+
+    protected $connection = 'pgsql_hris';
+    protected $primaryKey = 'nik';
+
+    protected $fillable = [
+        'nik',
+        'nama_karyawan',
+        'position_id',
+        'grade_id',
+        'costing',
+        'activity',
+        'tipe_kontrak',
+        'tgl_bergabung',
+        'tgl_pengangkatan',
+        'resign_date',
+        'tgl_naik_level',
+        'tgl_mcu_terakhir',
+        'email_perusahaan',
+        'point_of_hire',
+        'point_of_leave',
+        'ring_clasification',
+        'tipe_mess',
+        'martial_status_id',
+        'status'
+    ];
+
+
+    public function position()
+    {
+        return $this->hasOne(Position::class, 'position_id', 'position_id');
+    }
+}
---
+++ app/Models/Project.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+    use HasFactory;
+    protected $connection = 'pgsql_hris';
+    protected $primaryKey = 'project_id';
+
+    protected $fillable = [
+        'project_id',
+        'project',
+        'project_prefix',
+        'location',
+        'location_prefix',
+        'pjo',
+    ];
+
+    public function employee()
+    {
+        return $this->belongsTo(Employee::class, 'pjo', 'nik');
+    }
+
+    public function departments()
+    {
+        return $this->hasMany(Department::class, 'project_id', 'project_id');
+    }
+}
