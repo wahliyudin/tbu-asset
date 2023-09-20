3a50a606656e9ea4bf94b4f3f47dcb7b04dd49ac
wahliyudin <wahliyudinzein@gmail.com>
2023-09-20 14:09:08 +0700

    fix(Asset): fix bug;

--- resources/views/assets/asset/show-scan.blade.php
+++ resources/views/assets/asset/show-scan.blade.php
@@ -7,6 +7,7 @@
     <meta charset="utf-8" />
     <link rel="shortcut icon" href="{{ asset('assets/logo.png') }}" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="viewport" content="width=device-width, initial-scale=1"  />
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700" />
     <link href="{{ asset('assets/plugins/global/plugins.bundle.css') }}" rel="stylesheet" type="text/css" />
     <link href="{{ asset('assets/css/style.bundle.css') }}" rel="stylesheet" type="text/css" />
